306ae365a5ca6e6603fc4ada88a9298c785652f5
toujames <toujames@jamess-mbp.osustudent.okstate.edu>
2017-04-15 01:43:40 -0500

    Clean and Commented

--- Code_With_Button.ino
+++ Code_With_Button.ino
@@ -1,17 +1,14 @@
 /*
-fht_adc.pde
-guest openmusiclabs.com 9.5.12
-example sketch for testing the fht library.
-it takes in data on ADC0 (Analog0) and processes them
-with the fht. the data is sent out over the serial
-port at 115.2kb.  there is a pure data patch for
-visualizing the data.
+ * Reads in FFT Max data when button pressed
+ * Outputs the speed when button is released
 */
 
-#define LOG_OUT 1 // use the log output function
-#define FHT_N 256 // set to 256 point fht
+// Definitions
+#define LOG_OUT 1         // use the log output function
+#define FHT_N 256         // set to 256 point fht
 
-#include <FHT.h> // include the library
+// Liraries
+#include <FHT.h>          // include the library
 #include "Wire.h"
 #include "Adafruit_LiquidCrystal.h"
 
@@ -22,21 +19,12 @@ int max_mag = 180;                        // magnitude of bin threshold
 int bin_multiplier = 146.05;              // samplerate/fht. bin multiplier
 double highest_freq = 0;                             // Storing the highest Frequency
 
-int freq = 0;
-int largest = 0;
 int buttonState = HIGH;                  // button state is initialized as "pressed"
 
-byte arrow1[8] = {
-  B10000,
-  B11000,
-  B11100,
-  B11110,
-  B11100,
-  B11000,
-  B10000,
-  B00000
-};
-
+// Arrow Character
+byte arrow1[8] = { B10000, B11000, B11100, 
+                   B11110, B11100, B11000, 
+                   B10000,B00000 };
 
 double toMilesPerHour(double dopplerFreq){
   /*Doppler Formula
@@ -48,9 +36,20 @@ double toMilesPerHour(double dopplerFreq){
   return (dopplerFreq * 299792458.0 * 3600.0 ) / (2.0 * 24125000000.0 * 1609.0);
 }
 
+// method to get max of two values.
 int maxValue(int x, int y){
   if(x >= y) return x;
   return y;
+}
+
+// method to account for the bouncing during the button press.
+boolean debounceButton(boolean state){
+  boolean stateNow = digitalRead(9);
+  if(state!=stateNow){
+    delay(10);                                    // waits 10 ms to to account for the bouncing
+    stateNow = digitalRead(9);
+  }
+  return stateNow;
 }
 
 void setup() {
@@ -59,81 +58,76 @@ void setup() {
   lcd.begin(16, 2);                 // LCD Initialization       
   lcd.setBacklight(HIGH);           // LCD Backlight
   lcd.createChar(1, arrow1);
-  TIMSK0 = 0; // turn off timer0 for lower jitter
-  ADCSRA = 0xe5; // set the adc to free running mode
-  ADMUX = 0x40; // use adc0
-  DIDR0 = 0x01; // turn off the digital input for adc0
+  TIMSK0 = 0;                       // turn off timer0 for lower jitter
+  ADCSRA = 0xe5;                    // set the adc to free running mode
+  ADMUX = 0x40;                     // use adc0
+  DIDR0 = 0x01;                     // turn off the digital input for adc0
 }
 
 void loop() {
-
- while(1) {  // reduces jitter
-  //lcd.setCursor(0,0); lcd.print(highest_freq);
-
-    cli();  // UDRE interrupt slows this way down on arduino1.0
+ while(1) {                         // reduces jitter
+    cli();                          // UDRE interrupt slows this way down on arduino1.0
     for (int i = 0 ; i < FHT_N ; i++) { // save 256 samples
-      while(!(ADCSRA & 0x10) ); // wait for adc to be ready
-      ADCSRA = 0xf5; // restart adc
-      byte m = ADCL; // fetch adc data
+      while(!(ADCSRA & 0x10) );     // wait for adc to be ready
+      ADCSRA = 0xf5;                // restart adc
+      byte m = ADCL;                // fetch adc data
       byte j = ADCH;
-      int k = (j << 8) | m; // form into an int
-      k -= 0x0200; // form into a signed int
-      k <<= 6; // form into a 16b signed int
-      fht_input[i] = k; // put real data into bins
+      int k = (j << 8) | m;         // form into an int
+      k -= 0x0200;                  // form into a signed int
+      k <<= 6;                      // form into a 16b signed int
+      fht_input[i] = k;             // put real data into bins
     }
 
-    //
-
     /*
-     * fht_input[i], i is the frequency bin
-     * Frequency: f(i) = i * sample_rate / FHT_N
-     * sample_rate = 16 Mhz / 
+     * fht_log_out[i], 
+     *    i is the frequency bin
+     *    fht_log_out[i] is the magnitied
+     * ffrequency: f(i) = i * sample_rate / FHT_N
+     * sample_rate = 16 Mhz / some prescaler 
      */
-    fht_window(); // window the data for better frequency response
-    fht_reorder(); // reorder the data before doing the fht
-    fht_run(); // process the data in the fht
-    fht_mag_log(); // take the output of the fht
-    sei();
-    Serial.write(255); // send a start byte
-    Serial.write(fht_log_out, FHT_N/2); // send out the data
+    fht_window();       // window the data for better frequency response
+    fht_reorder();      // reorder the data before doing the fht
+    fht_run();          // process the data in the fht
+    fht_mag_log();      // take the output of the fht
+    sei();              // sets back interrupt
+
+    // Writes to Serial to shue graphic spetrum analyzer
+    Serial.write(255);                      // send a start byte
+    Serial.write(fht_log_out, FHT_N/2);     // send out the data
+
     
-    // with button!
-
-
-      int largest_index = 0;
-      int last = 0;
-      for (byte i = 2; i < FHT_N/2; i++) {                   // iterates through each bin and if is the largest it stores it. 
-        if( fht_log_out[i] >= max_mag) {
-          largest_index = i;
-        }
-      }// end of for loop
-      last = largest_index;
-  
-      double cur_freq = maxValue(last,largest_index) * bin_multiplier;
-  
-      if(cur_freq > highest_freq ) {
-        highest_freq = cur_freq;
+    int largest_index = 0;
+    int last = 0;
+    for (byte i = 2; i < FHT_N/2; i++) {    // iterates through each bin and if is the largest it stores it. 
+      if( fht_log_out[i] >= max_mag) {
+        largest_index = i;
       }
-      if(debounceButton(buttonState)==HIGH && buttonState == LOW){
-        highest_freq = 0;
-        lcd.setCursor(0,0); lcd.print("Collecting       ");
-        lcd.setCursor(0,1); lcd.write(1);lcd.write(1);lcd.write(1);lcd.write(1); lcd.write(1);lcd.write(1);lcd.print("       ");
-        //lcd.setCursor(0,1); lcd.print(" Rec               ");
-        buttonState = HIGH;
-    } else if ( debounceButton(buttonState)==LOW && buttonState == HIGH){
+    }
+    last = largest_index;                   // sets the last largest_index
+
+    // takes the two values and gets max and multiplies by a multiplier to get freq
+    double cur_freq = maxValue(last,largest_index) * bin_multiplier; 
+
+    // sets the highest_freq during reading to get the maximum value
+    if(cur_freq > highest_freq ) {
+      highest_freq = cur_freq;
+    }
+
+    // Code for button pressed. Sets the highest_freq to 0 and display words
+    if(debounceButton(buttonState)==HIGH && buttonState == LOW){
+      highest_freq = 0;
+      lcd.setCursor(0,0); lcd.print("Collecting       ");
+      lcd.setCursor(0,1); lcd.write(1);lcd.write(1);lcd.write(1);lcd.write(1); lcd.write(1);lcd.write(1);lcd.print("       ");
+      //lcd.setCursor(0,1); lcd.print(" Rec               ");
+      buttonState = HIGH;
+    } 
+    
+    // Code for when button released. prints the data to lcd
+    else if ( debounceButton(buttonState)==LOW && buttonState == HIGH){
       lcd.setCursor(0,0); lcd.print(highest_freq); lcd.print(" Hz        ");
       lcd.setCursor(0,1); lcd.print(toMilesPerHour(highest_freq)); lcd.print(" MPH        ");
       //lcd.setCursor(0,1); lcd.print(highest_freq); lcd.print(" Hz        ");
       buttonState = LOW;
     }
  }// end of while
-}
-
-boolean debounceButton(boolean state){
-  boolean stateNow = digitalRead(9);
-  if(state!=stateNow){
-    delay(10);                                    // waits 10 ms to to account for the bouncing
-    stateNow = digitalRead(9);
-  }
-  return stateNow;
 }
